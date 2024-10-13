76e887133b877757f9966853bfdc360f8c9d3619
Sodiq Al-ameen <sodiq@tm30.net>
2024-10-13 15:46:21 +0100

    see

--- components/SelectAndText.tsx
+++ components/SelectAndText.tsx
@@ -4,6 +4,7 @@ import { FlatList } from "react-native";
 import downcarat from "../assets/downcarat.png";
 import eng from "../assets/eng.png";
 import { TextInput } from "react-native";
+import { BlurView } from "expo-blur";
 
 const optionSelect = ["Option 1"];
 const options = ["Option 1", "Option 2", "Option 3"];
@@ -74,47 +75,63 @@ const SelectAndText = ({ title }: SelectAndTextProps) => {
               className="flex-row z-1000"
               style={{
                 paddingHorizontal: width * 0.1,
-                marginTop: height * 0.01,
+                marginTop: height * 0.01
               }}
             >
-              <FlatList
-                data={options}
-                keyExtractor={(item) => item}
-                className="rounded shadow-lg"
-                renderItem={({ item }) => (
-                  <TouchableOpacity onPress={() => handleSelect(item)}>
-                    <View
-                      style={{
-                        height: height * 0.05,
-                        borderWidth: 1,
-                        width: width * 0.21,
-                        // borderTopLeftRadius: 10,
-                        // borderBottomLeftRadius: 10,
-                        paddingLeft: 2,
-                        paddingTop: 3,
-                        flexDirection: "row", // Use flexDirection here
-                        alignItems: "center", // Align items vertically
-                        justifyContent: "center"
-                      }}
-                      className="flex-row text-textinputtext items-center text-[14px] border-customgray justify-center bg-primary"
-                    >
-                      <Image source={eng} />
-                      <Text
-                        style={{ marginHorizontal: 2 }}
-                        className="text-white"
-                      >
-                        Eng
-                      </Text>
-                      <Image source={downcarat} />
-                    </View>
-                  </TouchableOpacity>
-                )}
+              <BlurView
+              
                 style={{
                   maxHeight: 150,
                   position: "absolute", // Absolute positioning
+                  left: 0,
+                  right: 0,
                   zIndex: 1001 // Ensure it appears above other components
                 }}
-              />
+                reducedTransparencyFallbackColor="gray"
+                intensity={100} // Adjust the intensity of the blur
+                tint="dark" // You can use 'light', 'dark', or 'default'
+              >
+                <FlatList
+                  data={options}
+                  keyExtractor={(item) => item}
+                  className="rounded shadow-lg"
+                  renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => handleSelect(item)}>
+                      <View
+                        style={{
+                          height: height * 0.05,
+                          borderWidth: 1,
+                          width: width * 0.21,
+                          // borderTopLeftRadius: 10,
+                          // borderBottomLeftRadius: 10,
+                          paddingLeft: 2,
+                          paddingTop: 3,
+                          flexDirection: "row", // Use flexDirection here
+                          alignItems: "center", // Align items vertically
+                          justifyContent: "center"
+                        }}
+                        className="flex-row text-textinputtext items-center text-[14px] border-customgray justify-center bg-primary"
+                      >
+                        <Image source={eng} />
+                        <Text
+                          style={{ marginHorizontal: 2 }}
+                          className="text-white"
+                        >
+                          Eng
+                        </Text>
+                        <Image source={downcarat} />
+                      </View>
+                    </TouchableOpacity>
+                  )}
+                  style={{
+                    maxHeight: 150,
+                    position: "absolute", // Absolute positioning
+                    left: 0,
+                    right: 0,
+                    zIndex: 1001 // Ensure it appears above other components
+                  }}
+                />
+              </BlurView>
             </View>
           )}
         </View>
